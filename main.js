195ef0450bbedaf9d0a65c152a540c5f1f6eff16
John Cooley <johndavidcooley@gmail.com>
2016-03-01 23:25:56 -0500

    Dried up the JS with a function.
    Also made some small changes to the HTML and CSS to compare my calculator to a more sophisticated machine. I like mine better because I didn't have to buy it.

--- main.js
+++ main.js
@@ -1,100 +1,30 @@
-var one, two, three, four, five;
-var six, seven, eight, nine, zero;
-var plus, minus, multiply, divide;
-var decimal, equals, display, clear;
+var one = document.querySelector('#one');
+var two = document.querySelector('#two');
+var three = document.querySelector('#three');
+var four = document.querySelector('#four');
+var five = document.querySelector('#five');
+var six = document.querySelector('#six');
+var seven = document.querySelector('#seven');
+var eight = document.querySelector('#eight');
+var nine = document.querySelector('#nine');
+var zero = document.querySelector('#zero');
+var plus = document.querySelector('#plus');
+var minus = document.querySelector('#minus');
+var multiply = document.querySelector('#multiply');
+var divide = document.querySelector('#divide');
+var decimal = document.querySelector('#decimal');
+var equals = document.querySelector('#equals');
+var display = document.querySelector('#display');
+var clear = document.querySelector('#clear');
 
-one = document.querySelector('#one');
-two = document.querySelector('#two');
-three = document.querySelector('#three');
-four = document.querySelector('#four');
-five = document.querySelector('#five');
-six = document.querySelector('#six');
-seven = document.querySelector('#seven');
-eight = document.querySelector('#eight');
-nine = document.querySelector('#nine');
-zero = document.querySelector('#zero');
-plus = document.querySelector('#plus');
-minus = document.querySelector('#minus');
-multiply = document.querySelector('#multiply');
-divide = document.querySelector('#divide');
-decimal = document.querySelector('#decimal');
-equals = document.querySelector('#equals');
-display = document.querySelector('#display');
-clear = document.querySelector('#clear');
-
-function logOne() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "1";
-}
-
-function logTwo() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "2";
-}
-
-function logThree() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "3";
-}
-
-function logFour() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "4";
-}
-
-function logFive() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "5";
-}
-
-function logSix() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "6";
-}
-
-function logSeven() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "7";
-}
-
-function logEight() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "8";
-}
-
-function logNine() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "9";
-}
-
-function logZero() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "0";
-}
-
-function logPlus() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "+";
-}
-
-function logMinus() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "-";
+function logToDisplay() {
+	var toTheDisplay = event.currentTarget.textContent;
+	display.textContent =  display.textContent + toTheDisplay;
 }
 
 function logMultiply() {
 	// var toTheDisplay = event.currentTarget;
 	display.textContent =  display.textContent + "*";
-}
-
-function logDivide() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + "/";
-}
-
-function logDecimal() {
-	// var toTheDisplay = event.currentTarget;
-	display.textContent =  display.textContent + ".";
 }
 
 function logClear() {
@@ -105,20 +35,20 @@ function logEquals() {
 	display.textContent = eval(display.textContent);
 }
 
-one.addEventListener('click', logOne);
-two.addEventListener ('click', logTwo);
-three.addEventListener ('click', logThree);
-four.addEventListener ('click', logFour);
-five.addEventListener ('click', logFive);
-six.addEventListener ('click', logSix);
-seven.addEventListener ('click', logSeven);
-eight.addEventListener ('click', logEight);
-nine.addEventListener ('click', logNine);
-zero.addEventListener ('click', logZero);
-plus.addEventListener ('click', logPlus);
-minus.addEventListener ('click', logMinus);
+one.addEventListener('click', logToDisplay);
+two.addEventListener ('click', logToDisplay);
+three.addEventListener ('click', logToDisplay);
+four.addEventListener ('click', logToDisplay);
+five.addEventListener ('click', logToDisplay);
+six.addEventListener ('click', logToDisplay);
+seven.addEventListener ('click', logToDisplay);
+eight.addEventListener ('click', logToDisplay);
+nine.addEventListener ('click', logToDisplay);
+zero.addEventListener ('click', logToDisplay);
+plus.addEventListener ('click', logToDisplay);
+minus.addEventListener ('click', logToDisplay);
 multiply.addEventListener ('click', logMultiply);
-divide.addEventListener ('click', logDivide);
-decimal.addEventListener ('click', logDecimal);
+divide.addEventListener ('click', logToDisplay);
+decimal.addEventListener ('click', logToDisplay);
 clear.addEventListener ('click', logClear);
 equals.addEventListener ('click', logEquals);
